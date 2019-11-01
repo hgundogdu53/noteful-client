bf7e8f7383fdb7b6f7468d69a45c137d9a26dddc
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-10-31 21:00:02 -0700

    some test files removed

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -17,8 +17,8 @@ export default class NoteListMain extends React.Component {
 
   render() {
     const { folderId } = this.props.match.params
-    const { notes=[] } = this.context
-    const notesForFolder = getNotesForFolder(notes, folderId)
+    const { notes = [] } = this.context
+    const notesForFolder = getNotesForFolder(notes, parseInt(folderId))
     return (
       <section className='NoteListMain'>
         <ul>
@@ -26,7 +26,7 @@ export default class NoteListMain extends React.Component {
             <li key={note.id}>
               <Note
                 id={note.id}
-                name={note.name}
+                name={note.note_name}
                 modified={note.modified}
               />
             </li>
